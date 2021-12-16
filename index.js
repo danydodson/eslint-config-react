0d14b2cbdaecf4a9f13fe33c580e406d8a7287b2
Dany Dodson <danydodson@gmail.com>
2021-12-15 23:17:00 -0600

    feat: :wrench: initial commit

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
-    jest: true,
+    jest: true
   },
-  extends: ['prettier', 'plugin:react/recommended',],
+  extends: ['prettier', 'plugin:react/recommended'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2020,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['prettier', 'react', 'react-hooks', 'jsx-a11y'],
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
   ignorePatterns: ['node_modules/'],
   rules: {
@@ -36,9 +36,9 @@ module.exports = {
     'newline-per-chained-call': 0,
     'no-confusing-arrow': 0,
     'no-console': 1,
-    'no-unused-vars': 2,
+    'no-unused-vars': 0,
     'no-use-before-define': 0,
-    'prefer-template': 2,
+    'react/prop-types': 0,
     'react/destructuring-assignment': 0,
     'react/require-default-props': 0,
     'react/require-extension': 0,
@@ -46,26 +46,19 @@ module.exports = {
     'react/sort-comp': 0,
     'react/forbid-prop-types': 0,
     'react/jsx-closing-tag-location': 0,
-    'react/jsx-first-prop-new-line': [2, 'multiline'],
     'react/jsx-filename-extension': 0,
     'react/jsx-props-no-spreading': 0,
     'react/jsx-no-target-blank': 0,
     'react/jsx-uses-vars': 2,
+    'react-hooks/rules-of-hooks': 2,
     'jsx-a11y/aria-props': 2,
     'jsx-a11y/heading-has-content': 0,
-    'jsx-a11y/label-has-associated-control': [
-      2,
-      {
-        // NOTE: If this error triggers, either disable it or add
-        // your custom components, labels and attributes via these options
-        // See https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-associated-control.md
-        controlComponents: ['Input'],
-      },
-    ],
     'jsx-a11y/label-has-for': 0,
     'jsx-a11y/mouse-events-have-key-events': 2,
     'jsx-a11y/role-has-required-aria-props': 2,
     'jsx-a11y/role-supports-aria-props': 2,
-    'react-hooks/rules-of-hooks': 2,
-  },
+    // If this error triggers, either disable it or add your custom components to controlComponents
+    // See https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-associated-control.md
+    'jsx-a11y/label-has-associated-control': [2, { controlComponents: ['Input'] }]
+  }
 }
