24e21507321dd23f0a76bb6256b5d26868ed34b3
Dany Dodson <danydodson@gmail.com>
2021-12-15 21:11:18 -0600

    feat: :wrench: initial commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ['airbnb', 'prettier', 'prettier/react'],
+  extends: ['prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
